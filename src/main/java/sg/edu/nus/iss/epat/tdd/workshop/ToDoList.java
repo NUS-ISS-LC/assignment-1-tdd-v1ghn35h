b5647b704f35013606836975d802fd8d719ba655
Vighnesh <107790137+v1ghn35h@users.noreply.github.com>
2025-11-04 03:18:22 +0000

    implement toDoList and toDoListTest

--- src/main/java/sg/edu/nus/iss/epat/tdd/workshop/ToDoList.java
+++ src/main/java/sg/edu/nus/iss/epat/tdd/workshop/ToDoList.java
@@ -1,31 +1,38 @@
 package sg.edu.nus.iss.epat.tdd.workshop;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class ToDoList {
    private HashMap<String, Task> tasks = new HashMap<String, Task>();
 
    public void addTask(Task task) {
       // Add code here
+      this.tasks.put(task.getDescription(), task);
    }
 
    public void completeTask(String description) {
       // Add code here
+      Task task = tasks.get(description);
+      if (task != null) {
+         task.setComplete(true);
+      }
    }
 
    public boolean getStatus(String description) {
-      // Add code here
+      Task task = tasks.get(description);
+      if (task != null) {
+         return task.isComplete();
+      }
       return false;
    }
 
    public Task getTask(String description) {
-      // Add code here
-      return null;
+      return tasks.get(description);
    }
 
    public Task removeTask(String description) {
-      // Add code here
-      return null;
+      return tasks.remove(description);
    }
 
    public Collection<Task> getAllTasks() {
@@ -33,7 +40,9 @@ public class ToDoList {
    }
 
    public Collection<Task> getCompletedTasks() {
-      // Add code here
-      return null;
-   }
+      return tasks.values()
+            .stream()
+            .filter(Task::isComplete)
+            .collect(Collectors.toList());
+      }
 }
--- src/test/java/sg/edu/nus/iss/epat/tdd/workshop/ToDoListTest.java
+++ src/test/java/sg/edu/nus/iss/epat/tdd/workshop/ToDoListTest.java
@@ -3,11 +3,21 @@ package sg.edu.nus.iss.epat.tdd.workshop;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+
 public class ToDoListTest  {
     // Define Test Fixtures
+
+    private Task task1;
+    private Task task2;
+    private ToDoList toDoList;
 
     public ToDoListTest() {
         super();
@@ -16,6 +26,9 @@ public class ToDoListTest  {
     @Before
     public void setUp() throws Exception {
         // Initialise Test Fixtures
+        toDoList = new ToDoList();
+        task1 = new Task("Task 1");
+        task2 = new Task("Task 2");
     }
 
     @After
@@ -25,21 +38,41 @@ public class ToDoListTest  {
 
     @Test
     public void testAddTask() {
-        fail("Not implemented yet");
+        toDoList.addTask(task1);
+
+        assertTrue(toDoList.getAllTasks().contains(task1));
     }
 
     @Test
     public void testGetStatus() {
-        fail("Not implemented yet");
+        toDoList.addTask(task1);
+        assertFalse(toDoList.getStatus("Task 1"));
+
+        toDoList.completeTask("Task 1");
+        assertTrue(toDoList.getStatus("Task 1"));
     }
 
     @Test
     public void testRemoveTask() {
-        fail("Not implemented yet");
+        toDoList.addTask(task1);
+        toDoList.addTask(task2);
+
+        Task removed = toDoList.removeTask("Task 1");
+        assertEquals(task1, removed);
+        assertFalse(toDoList.getAllTasks().contains(task1));
+        assertTrue(toDoList.getAllTasks().contains(task2)); 
     }
 
     @Test
     public void testGetCompletedTasks() {
-        fail("Not implemented yet");
+        toDoList.addTask(task1);
+        toDoList.addTask(task2);
+
+        toDoList.completeTask("Task 2");
+
+        Collection<Task> completedTasks = toDoList.getCompletedTasks();
+        assertTrue(completedTasks.contains(task2));
+        assertFalse(completedTasks.contains(task1));
+        assertEquals(1, completedTasks.size());
     }
 }
